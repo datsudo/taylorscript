9f90dadfc1fd80d454a3b54c559a6a01688232cb
datsudo <76833632+datsudo@users.noreply.github.com>
2023-10-10 01:46:01 +0800

    Parser: Add method for parsing tokens in script file

--- src/main/java/com/ppl/minipl/Parser.java
+++ src/main/java/com/ppl/minipl/Parser.java
@@ -8,8 +8,25 @@ import static com.ppl.minipl.TokenType.*;
 public class Parser {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int lineNumber = 1;
 
     Parser(String source) {
         this.source = source;
+    }
+
+    List<Token> parseTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            parseToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, lineNumber));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
     }
 }
