a5b79b6e1e2acec563bfff11225f83c8dabb4822
datsudo <76833632+datsudo@users.noreply.github.com>
2023-10-10 02:15:32 +0800

    Fix: Import nio.file.Paths; fix report var name

--- src/main/java/com/ppl/minipl/PL.java
+++ src/main/java/com/ppl/minipl/PL.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class PL {
@@ -59,7 +60,7 @@ public class PL {
     }
 
     private static void report(int lineNumber, String where, String message) {
-        System.err.println("[LINE " + line + "] Error" + where + ": " + message);
+        System.err.println("[LINE " + lineNumber + "] Error" + where + ": " + message);
         hadError = true;
     }
 }
