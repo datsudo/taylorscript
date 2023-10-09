683523bd051ef00a8310a4711016eb754fce545c
datsudo <76833632+datsudo@users.noreply.github.com>
2023-10-10 01:51:14 +0800

    Parser: Initialize parseToken method
    * currently handles single-char tokens
    * add advance(): consumes char
    * add addToken(): add tokens in Parser's tokens list

--- src/main/java/com/ppl/minipl/Parser.java
+++ src/main/java/com/ppl/minipl/Parser.java
@@ -26,7 +26,43 @@ public class Parser {
         return tokens;
     }
 
+    private void parseToken() {
+        // parse individual token
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LPAREN   ); break;
+            case ')': addToken(RPAREN   ); break;
+            case '{': addToken(LBRACE   ); break;
+            case '}': addToken(RBRACE   ); break;
+            case '-': addToken(MINUS    ); break;
+            case '+': addToken(PLUS     ); break;
+            case '*': addToken(STAR     ); break;
+            case ',': addToken(COMMA    ); break;
+            case '.': addToken(DOT      ); break;
+            case ';': addToken(SEMICOLON); break;
+
+            default:
+                PL.error(lineNumber, "Unexpected character.");
+                break;
+        }
+    }
+
     private boolean isAtEnd() {
         return current >= source.length();
+    }
+
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        // this overload method is for tokens with literal values
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, lineNumber));
     }
 }
