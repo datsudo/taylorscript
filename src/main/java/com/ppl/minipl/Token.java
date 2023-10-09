9968c72d0e931a0d8315aa9b2fb070033916e65b
datsudo <76833632+datsudo@users.noreply.github.com>
2023-10-10 01:40:18 +0800

    Create Token blueprint

---
+++ src/main/java/com/ppl/minipl/Token.java
@@ -0,0 +1,19 @@
+package com.ppl.minipl;
+
+class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int lineNumber;
+
+    Token(TokenType type, String lexeme, Object literal, int lineNumber) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.lineNumber = lineNumber;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
