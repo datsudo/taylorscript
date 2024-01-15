8da70b751584aa65efce05aa7bb5dfcf9dd03566
datsudo <76833632+datsudo@users.noreply.github.com>
2024-01-15 17:06:05 +0800

    Update batch file

--- taylorscript.bat
+++ taylorscript.bat
@@ -1,1 +1,13 @@
-mvn initialize & mvn compile & cls & java -cp "target\classes;lib/*" com.taylorscript.main.TaylorScript
+@ECHO off
+SET /p runOpt="Run REPL [r] or a script file [insert filepath]: "
+
+call mvn initialize
+call mvn compile
+call cls
+
+IF %runOpt%==r (
+    java -cp "target\classes;lib/*" com.taylorscript.main.TaylorScript
+) ELSE (
+    java -cp "target\classes;lib/*" com.taylorscript.main.TaylorScript %runOpt%
+    pause >nul
+)
