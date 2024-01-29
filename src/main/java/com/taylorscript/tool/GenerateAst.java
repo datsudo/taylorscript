b443636800a9c390858024d752d459ae2d8ea20b
datsudo <76833632+datsudo@users.noreply.github.com>
2024-01-29 20:44:04 +0800

    Add new node for stmt and expr
    - For expr: node for accessing variable
    - For stmt: node for declaring variables

--- src/main/java/com/taylorscript/tool/GenerateAst.java
+++ src/main/java/com/taylorscript/tool/GenerateAst.java
@@ -17,12 +17,14 @@ public class GenerateAst {
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
-                "Unary    : Token operator, Expr right"
+                "Unary    : Token operator, Expr right",
+                "Variable : Token name"
         ));
 
         defineAst(outputDir, "Statement", Arrays.asList(
                 "Expression : Expr expression",
-                "Print      : Expr expression"
+                "Print      : Expr expression",
+                "Let        : Token name, Expr initializer"
         ));
     }
 
