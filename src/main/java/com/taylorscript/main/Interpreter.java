8b2137c6ca6a05b4839a3b0ca991bb3ba6e4ad00
datsudo <76833632+datsudo@users.noreply.github.com>
2024-01-29 18:49:04 +0800

    Add comments for visitLiteralExpr

--- src/main/java/com/taylorscript/main/Interpreter.java
+++ src/main/java/com/taylorscript/main/Interpreter.java
@@ -3,6 +3,7 @@ package com.taylorscript.main;
 class Interpreter implements Expr.Visitor<Object> {
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
+        // Returns the literal value of literal expression
         return expr.value;
     }
 }
