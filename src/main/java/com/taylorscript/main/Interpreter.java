7cc0cd24b01492d51fd1ce75337ff9ecb4a5080a
datsudo <76833632+datsudo@users.noreply.github.com>
2024-01-29 19:03:28 +0800

    Implement parser for unary exprs

--- src/main/java/com/taylorscript/main/Interpreter.java
+++ src/main/java/com/taylorscript/main/Interpreter.java
@@ -8,6 +8,28 @@ class Interpreter implements Expr.Visitor<Object> {
     }
 
     @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        // evaluate first the operand before the unary operator
+        Object rightSubExpr = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(rightSubExpr);
+            case MINUS:
+                return -(double)rightSubExpr;
+        }
+
+        return null;
+    }
+
+    @Override
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         // Evaluates grouping in expressions
         return evaluate(expr.expression);
