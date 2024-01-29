ce1caeafcb25b1217c096638df05d64e35fc76a7
datsudo <76833632+datsudo@users.noreply.github.com>
2024-01-29 22:31:44 +0800

    Add parser for assignment stmt

--- src/main/java/com/taylorscript/main/Parser.java
+++ src/main/java/com/taylorscript/main/Parser.java
@@ -71,6 +71,24 @@ class Parser {
         return new Statement.Expression(expr);
     }
 
+    private Expr assignment() {
+        Expr expr = equality();
+
+        if (match(EQUAL)) {
+            Token equals = previous();
+            Expr value = assignment();
+
+            if (expr instanceof Expr.Variable) {
+                Token name = ((Expr.Variable)expr).name;
+                return new Expr.Assign(name, value);
+            }
+
+            error(equals, "Invalid assignment target.");
+        }
+
+        return expr;
+    }
+
     private Expr equality() {
         // equality -> comparison ( ( "!=" | "==" ) comparison )*
         Expr expr = comparison();
