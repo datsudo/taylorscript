ab859bc82397928bece26d5ea1f1377caba52e96
datsudo <76833632+datsudo@users.noreply.github.com>
2024-01-31 01:10:06 +0800

    Add clock() native function

--- src/main/java/com/taylorscript/main/Interpreter.java
+++ src/main/java/com/taylorscript/main/Interpreter.java
@@ -4,7 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Statement.Visitor<Void> {
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
+    Interpreter() {
+        globals.define("clock", TSCallable() {
+            @Override
+            public int arity() { return 0; }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> args) {
+                return (double)System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() { return "<native fn>"; }
+        });
+    }
 
     void interpret(List<Statement> statements) {
         try {
